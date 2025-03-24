cd3e93fee8f8567c3ffcb5ee633f2e585e494ffd
Your Name <you@example.com>
2025-03-25 00:36:50 +0700

    chua co gi ca

---
+++ src/main/java/com/crudstring/ProjectCrudspringApplication.java
@@ -0,0 +1,13 @@
+package com.crudstring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCrudspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCrudspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudstring/ProjectCrudspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudstring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCrudspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
